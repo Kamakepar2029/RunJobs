5029a85c441114427a9f74c42d1bcffa0e4bf2b3
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-12-18 14:14:25 +0300

    Update kama.py

--- kama.py
+++ kama.py
@@ -4,10 +4,10 @@ class Kama():
   def __init__(self,folder):
     self.folder = folder
 
-  def render_k(self,template,ks):
+  def render_k(self,template,ks,stri):
     pill = open(self.folder+'/'+template,'r').read()
     args = json.loads(ks)
     stro = ''
     for key in args:
       pill = pill.replace('%'+key+'%',args[key])
-    return pill
+    return pill.replace('%stri%',stri)
